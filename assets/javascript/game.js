32b8a6801f931c18ac24e948fe0c08b9dd03654b
thebreezyp <patrickfinnigan94@gmail.com>
2018-06-12 15:10:45 -0700

    added more jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,10 +5,27 @@ $(document).ready(function() {
     var randomRed = 0;
     var randomYellow = 0;
     var totalGems = 0;
+    var wins = 0;
+    var losses = 0;
 
+    // random number for the gem goal
     randomGoal = Math.floor((Math.random() * 100) + 30);
     $("#target-number").text(randomGoal);
 
+    // a random number variable for each gem button
     randomBlue = Math.floor((Math.random() * 3) + 1);
     console.log(randomBlue);
+
+    randomGreen = Math.floor((Math.random() * 10) + 3);
+    console.log(randomGreen);
+
+    randomRed = Math.floor((Math.random() * 18) + 10);
+    console.log(randomRed);
+
+    randomYellow = Math.floor((Math.random() * 25) + 18);
+    console.log(randomYellow);
+
+    $(".blueBtn").on("click", function() {
+        alert("hey");
+    });
 });
