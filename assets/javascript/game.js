5bb1438e5740539d37b7ca81d5b55a5520a033b2
thebreezyp <patrickfinnigan94@gmail.com>
2018-06-12 21:52:17 -0700

    additional jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,86 @@
-$(document).ready(function() {
-    var randomGoal = 0;
-    var randomBlue = 0;
-    var randomGreen = 0;
-    var randomRed = 0;
-    var randomYellow = 0;
-    var totalGems = 0;
-    var wins = 0;
-    var losses = 0;
+var randomGoal = 0;
+var randomBlue = 0;
+var randomGreen = 0;
+var randomRed = 0;
+var randomYellow = 0;
+var totalGems = 0;
+var wins = 0;
+var losses = 0;
 
-    // random number for the gem goal
-    randomGoal = Math.floor((Math.random() * 100) + 30);
-    $("#target-number").text(randomGoal);
+$(document).ready(function () {
+    console.log(totalGems);
 
-    // a random number variable for each gem button
-    randomBlue = Math.floor((Math.random() * 3) + 1);
-    console.log(randomBlue);
+    var randomGen = function (range, offset) {
+        return Math.floor((Math.random() * range) + offset);
+    }
 
-    randomGreen = Math.floor((Math.random() * 10) + 3);
-    console.log(randomGreen);
+    function startGame() {
+        // random number for the gem goal
+        randomGoal = randomGen(100, 30)
+        $("#target-number").text(randomGoal);
 
-    randomRed = Math.floor((Math.random() * 18) + 10);
-    console.log(randomRed);
+        // a random number variable for each gem button
+        randomBlue = randomGen(1, 3);
+        console.log(randomBlue);
+        $(".blueBtn").attr("data-value", randomBlue);
+        // adding button value to DOM element
 
-    randomYellow = Math.floor((Math.random() * 25) + 18);
-    console.log(randomYellow);
+        randomGreen = randomGen(10, 3)
+        console.log(randomGreen);
+        $(".greenBtn").attr("data-value", randomGreen);
 
-    $(".blueBtn").on("click", function() {
-        alert("hey");
+        randomRed = randomGen(18, 10)
+        console.log(randomRed);
+        $(".redBtn").attr("data-value", randomRed);
+
+        randomYellow = randomGen(25, 18)
+        console.log(randomYellow);
+        $(".yellowBtn").attr("data-value", randomYellow);
+    }
+
+    startGame();
+
+    $(".blueBtn").on("click", function () {
+        var btnValue = $(this).data("value")
+        totalGems += btnValue;
+        console.log(totalGems);
+        $("#gemCount").text(totalGems);
+        checkStatus();
     });
+
+    $(".greenBtn").on("click", function () {
+        var btnValue = $(this).data("value")
+        totalGems += btnValue;
+        console.log(totalGems);
+        $("#gemCount").text(totalGems);
+        checkStatus();
+    });
+
+    $(".yellowBtn").on("click", function () {
+        var btnValue = $(this).data("value")
+        totalGems += btnValue;
+        console.log(totalGems);
+        $("#gemCount").text(totalGems);
+        checkStatus();
+    });
+
+    $(".redBtn").on("click", function () {
+        var btnValue = $(this).data("value")
+        totalGems += btnValue;
+        console.log(totalGems);
+        $("#gemCount").text(totalGems);
+        checkStatus();
+    });
+
+
+    function checkStatus() {
+        if (totalGems === randomGoal) {
+            alert("You Win!");
+            wins++;
+        } else if (totalGems > randomGoal) {
+            alert("You Lose!");
+            losses++;
+            console.log(losses);
+        }
+    }
 });
